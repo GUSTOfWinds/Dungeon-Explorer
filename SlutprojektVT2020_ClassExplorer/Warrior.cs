42861c5b4655f26a96aff80e4fd923c585bbde7c
GUSTOfWinds <gustavkjonsson01@gmail.com>
2020-03-26 10:36:30 +0100

    Warrior.cs Comments

--- SlutprojektVT2020_ClassExplorer/Warrior.cs
+++ SlutprojektVT2020_ClassExplorer/Warrior.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace SlutprojektVT2020_ClassExplorer
 {
+    //References directly to Hero, as this is a class used by the player, and thus it should be able to use any functions any player class would be able to use, which would be within the hero class
     class Warrior : Hero
     {
+        //A constructor for generating the "ability scores" of the hero character, called upon during the "character creation" process of program.cs
         public Warrior()
         {
             strength = 75;
