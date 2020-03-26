609d60526e4f63b91487b2ca914664e0755fedc0
GUSTOfWinds <gustavkjonsson01@gmail.com>
2020-03-26 10:25:37 +0100

    CorridorRoom.cs Comments Added

--- SlutprojektVT2020_ClassExplorer/CorridorRoom.cs
+++ SlutprojektVT2020_ClassExplorer/CorridorRoom.cs
@@ -6,6 +6,6 @@ namespace SlutprojektVT2020_ClassExplorer
 {
     class CorridorRoom : Room
     {
-            
+        //TO ADD: A method that generates options for the player, describing other possible places they can go to. This class is meant to serve as an intermediary between other rooms
     }
 }
