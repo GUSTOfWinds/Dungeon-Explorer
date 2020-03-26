8a7d67313ae233923e65d4f798b965b29077a3d7
GUSTOfWinds <gustavkjonsson01@gmail.com>
2020-03-26 10:33:11 +0100

    Monster.cs comments

--- SlutprojektVT2020_ClassExplorer/Monster.cs
+++ SlutprojektVT2020_ClassExplorer/Monster.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace SlutprojektVT2020_ClassExplorer
 {
+    //References creature, allowing it to use basic variables like ability scores as well as creature-generic methods, like being able to check ability scores
     class Monster : Creature
     {
-        //METHODS FOR ACTIONS THAT THE MONSTERS CAN DO
+        //TO ADD: Monsters-Specific "Actions" (methods), such as deciding what to do when encountering the hero
     }
 }
