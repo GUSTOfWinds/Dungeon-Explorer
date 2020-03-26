45bf7a312f7798dae5104887515522356476f42a
GUSTOfWinds <gustavkjonsson01@gmail.com>
2020-03-26 10:14:44 +0100

    First Version - Monstergenerator

--- SlutprojektVT2020_ClassExplorer/MonsterRoom.cs
+++ SlutprojektVT2020_ClassExplorer/MonsterRoom.cs
@@ -6,5 +6,18 @@ namespace SlutprojektVT2020_ClassExplorer
 {
     class MonsterRoom : Room
     {
+        int monsterAmount;
+        List<Monster> monsterList = new List<Monster>();
+
+        static void monsterGenerator()
+        {
+            monsterAmount = randomgenerator.Next(6);
+
+            for (int i = 0; i < monsterAmount; i++)
+            {
+                Goblin genericGoblin = new Goblin();
+                monsterList.Add(genericGoblin);
+            }
+        }
     }
 }
