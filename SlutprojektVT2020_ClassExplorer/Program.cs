8dc726c6428129a9142a3a8f73197e27cb2dce36
GUSTOfWinds <gustavkjonsson01@gmail.com>
2020-03-26 09:42:24 +0100

    Pre-Konstructor Fix

--- SlutprojektVT2020_ClassExplorer/Program.cs
+++ SlutprojektVT2020_ClassExplorer/Program.cs
@@ -8,7 +8,7 @@ namespace SlutprojektVT2020_ClassExplorer
         {
             Random randomgenerator = new Random();
 
-            //Character Creator that generates both a statblock for the hero, as well as a name. Both of these are chosen by the user.
+
             Console.WriteLine("Greetings, Hero. You are about to enter the dungeon. Before you enter, please select a name and class");
             Console.WriteLine("To begin with, please write in your name, and then hit enter to confirm");
             string HeroName = Console.ReadLine();
@@ -20,26 +20,26 @@ namespace SlutprojektVT2020_ClassExplorer
             if (HeroClass == "1")
             {
                 Console.WriteLine(HeroName + " is a Warrior!");
-                Warrior Hero = new Warrior();
             }
-            //MORE CLASSES
+
+
+
             else
             {
                 Console.WriteLine(HeroName + " is a Failure...");
-                Failure Hero = new Failure();
             }
             Console.ReadLine();
             Console.WriteLine("Now, into the dungeon you go. Good luck!");
             Console.ReadLine();
 
 
-            //TESTING AREA
+
             Console.WriteLine("A Goblin Appears!");
             Goblin TestEnemy = new Goblin();
             Console.ReadLine();
             Console.WriteLine("The Goblin & The Hero Fight!");
 
-            bool HeroWin = Hero.ContestStrength(TestEnemy.strength);
+            bool HeroWin = hero.ContestStrength(TestEnemy.strength);
             if (HeroWin == true)
             {
                 Console.WriteLine("Hero Wins!")
@@ -50,8 +50,6 @@ namespace SlutprojektVT2020_ClassExplorer
             }
             Console.WriteLine("TestEnd");
             Console.ReadLine();
-
-            //ACTUAL DUNGEON GAME
         }
     }
 }
