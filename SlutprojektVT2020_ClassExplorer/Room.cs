504fd0a3902cc1d855312aa1e303ff89f56c633c
GUSTOfWinds <gustavkjonsson01@gmail.com>
2020-03-26 10:36:06 +0100

    TreasureRoom.cs Comments

--- SlutprojektVT2020_ClassExplorer/Room.cs
+++ SlutprojektVT2020_ClassExplorer/Room.cs
@@ -6,6 +6,7 @@ namespace SlutprojektVT2020_ClassExplorer
 {
     class Room
     {
+        //A random generator to be used by any room
         public static Random randomgenerator = new Random();
     }
 }
--- SlutprojektVT2020_ClassExplorer/TreasureRoom.cs
+++ SlutprojektVT2020_ClassExplorer/TreasureRoom.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace SlutprojektVT2020_ClassExplorer
 {
+    //References Room so that any room functions can also be used by the treasureroom
     class TreasureRoom : Room
     {
-
+        //ADD: Method that randomly generates treasure for hero, randomly generates traps, checks how the hero deals with traps, etc.
     }
 }
